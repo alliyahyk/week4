0335f0a070cea117580851906d6fa3cea8ee8385
Nadine Lathifah <nadinehendronoto@gmail.com>
2025-02-11 22:30:34 +0300

    EDIT: files

---
+++ Exercise13/read_file.py
@@ -0,0 +1,35 @@
+#   1) Use open and read to slurp the entire contents of your pelican file.
+
+pelican_file = open('pelican.txt', 'r').read()
+
+# slurping          refers to the practice of reading an entire file or data stream into memory all at once,
+#                   rather than processing it line by line or in smaller chunks.
+#                   Used to load the entire contents of a file into a single variable.
+#                   Small files: efficient –> simplifies code, reduces need for loops.
+#                   Large files: inefficient –> consume a lot of memory.
+
+# streaming         The file is processed line by line.
+
+# .read()           reads entire file into a single string.
+
+# .readlines()      reads entire file into a list of strings, where each string represents a line.
+
+
+#   2) Display the data type and print contents of the returned data.
+
+print(f"Contents of pelican file:\n{pelican_file}")
+print(type(pelican_file))
+# Data type: string
+
+#   3) Read file into a list and then output the number of items within the list.
+
+pelican_list = open('pelican.txt', 'r').readlines()
+print(f"Reading Pelican file as a list:\n{pelican_list}")
+print(len(pelican_list))
+
+#   4) Use a loop to iterate over and print the contents of the file.
+
+for line in open('pelican.txt', 'r'):
+    print(line.strip())
+
+# .strip() removes the trailing \n from each file when printing. Ensures no blank spaces in the output.
---
+++ Exercise13/write_file.py
@@ -0,0 +1,26 @@
+#   1) Create a file handle to open and append to a file called pelican.txt
+file_handle = open('pelican.txt', 'a')
+
+# open(file name, mode)         Function opens a file and returns the file object (file handle) via two arguments (file name, mode)
+#                               Modes:
+#                                 'r': read; allows reading
+#                                 'w': write; overwrites the file
+#                                 'a': append; data is written to the end of the file, if file ≠ exist, it is created.
+
+# , comma is part of Python syntax to separate parameters in function calls.
+# file_handle is a variable that stores the file object returned by open().
+
+#   2) Append lines into the file.
+file_handle.write("A wonderful bird is the pelican")
+file_handle.write("His bill holds more than his belican\n")
+
+#   3) Create a list that contains sentences from exercise guide and append list to the file.
+lines = list("He can take in his beak,\nEnough food for a week,\nBut I'm damned if I see how the helican.\n")
+file_handle.writelines(lines)
+
+# .writelines()     is a method that writes a list of strings to a file
+# \n                newline is an escape sequence that represents a newline.
+#           purpose: ensures each line of text starts on a new line, readability, and helps with structuring, formatting in files.
+
+
+
